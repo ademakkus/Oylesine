ed9acb7d578839b140fce6ffa56ee5c7f7197030
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-11-26 16:10:50 +0300

    add -
    adding - sign between array's elements

---
+++ DiziElemanlariArasinaTireIsareti/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using  static System.Console;
+namespace DiziElemanlariArasinaTireIsareti
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char i = 'A'; i <='Z'; i++)
+            {
+                Write(i+(i<'Z'?"-":""));
+            }
+            WriteLine();
+            ReadKey();
+        }
+    }
+}
