db451f668d997f7adb4f27487ee826d033ef5b40
Adem AKKUŞ <32845253+ademakkus@users.noreply.github.com>
2019-12-06 12:34:16 +0300

    Tupples project addd

---
+++ Tupples/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tupples
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //(string, int, char) values = ("adem akuş", 10, 'c');//give the errror
+      (string, int, char) values = ("adem akuş", 10, 'c');
+      //var values=("adem akuş", 10, 'c');
+      Console.WriteLine($"First Item : {values.Item1}");
+      Console.WriteLine($"Second Item : {values.Item2}");
+      Console.WriteLine($"ThirdItem : {values.Item3}");
+      Console.ReadKey();
+    }
+  }
+}
